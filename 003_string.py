487a4a3ee1648c6ac919669064630fbcf6395061
5h4hid5 <shahid.bracu@gmail.com>
2017-03-02 20:10:46 +0600

    Added string file

---
+++ 003_string.py
@@ -0,0 +1,53 @@
+name = "Shahid";
+print(name)
+print('\n')
+
+#Escape quote
+quote = "Sufyān Al-Thawrī – Allāh have mercy on him – said, 'Knowledge is only learned to [apply it and] put into practice the Taqwā of Allāh the mighty and sublime.'"
+print(quote)
+print('\n')
+
+#Escape character
+myVoice = 'It\'s my pen'
+print(myVoice)
+print('\n')
+
+#Raw string
+rawString = r'home/new-folder/coding'
+print(rawString)
+print('\n')
+
+#String concatination
+firstName = 'Shahidul'
+lastName = 'Islam'
+fullName = firstName + ' ' + lastName
+print(fullName)
+print('\n')
+
+#Multiply string
+char = 'a'
+print(char*5)
+print('\n')
+
+#Accessing character
+fruit = "Mango"
+charN = fruit[2]
+print(charN)
+print('\n')
+
+#Last character
+tree = "Shimul"
+charLast = tree[-1]
+print(charLast)
+print('\n')
+
+#Slice atring
+fullText = "This is a text"
+slicedText = fullText[5:7]
+print(slicedText)
+print('\n')
+
+#Length of string
+myString = "This is a string"
+length = len(myString)
+print(length)
